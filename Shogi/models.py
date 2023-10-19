29c9c47b155ce25e717b736e416a5c35c8b449e8
Ron Chen <ucdj2003@gmail.com>
2023-10-19 09:41:31 +0800

    One to one Django User to Player

--- Shogi/models.py
+++ Shogi/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Player(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     victories = models.PositiveIntegerField(default=0)
     opponents = models.ManyToManyField(
